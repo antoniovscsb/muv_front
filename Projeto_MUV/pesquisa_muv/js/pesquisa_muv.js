5d817c083e1c9ee664b11b3b6d222e92b025bb5c
bacalhau2 <joaquimbarros2005@gmail.com>
2026-08-26 10:19:36 -0300

    cabecalho e pesquisa em js e css

---
+++ Projeto_MUV/pesquisa_muv/js/pesquisa_muv.js
@@ -0,0 +1,11 @@
+const pesquisa = document.querySelector('#pesquisa');
+
+pesquisa.innerHTML =`
+
+    <section class="pesquisa">
+        <input type="text" placeholder="Buscar produtos ou serviços...">
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+    </section>
+
+
+`;
